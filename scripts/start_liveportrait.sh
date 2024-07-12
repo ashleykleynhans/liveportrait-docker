ca988b750e607b74e9eeb80938f0d07e7714e9c6
Ashley Kleynhans <ashley.kleynhans@gmail.com>
2024-07-12 11:54:59 +0200

    Set server_name to 0.0.0.0

--- scripts/start_liveportrait.sh
+++ scripts/start_liveportrait.sh
@@ -6,7 +6,7 @@ cd /workspace/LivePortrait
 TCMALLOC="$(ldconfig -p | grep -Po "libtcmalloc.so.\d" | head -n 1)"
 export LD_PRELOAD="${TCMALLOC}"
 export PYTHONUNBUFFERED=1
-nohup python3 app.py --server_port 3001 > /workspace/logs/LivePortrait.log 2>&1 &
+nohup python3 app.py --server_name 0.0.0.0 --server_port 3001 > /workspace/logs/LivePortrait.log 2>&1 &
 echo "LivePortrait started"
 echo "Log file: /workspace/logs/LivePortrait.log"
 deactivate
